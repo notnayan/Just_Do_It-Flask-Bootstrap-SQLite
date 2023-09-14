6f3248b471479fee8057bac8df64beb4132bd7af
notnayan <nayanrajkhanalz@gmail.com>
2023-09-14 17:03:09 +0545

    Added SQLite database engine

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///justdoit.db'
+db = SQLAlchemy(app)
+
+
+class JustDoIt(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    desc = db.Column(db.String(300), nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
 @app.route('/')
-def hello_world():
+def just_do_it():
     return render_template("index.html")
 
+
+def create_database():
+    with app.app_context():
+        db.create_all()
+
+
 if __name__ == "__main__":
-    app.run(debug = True, port = 6969)
+    create_database()
+    app.run(debug=True, port=6969)
